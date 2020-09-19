6ac8d8d5552302b05efba1f69824ea7bc5a37d6e
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-19 23:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function getFirstSelector(selector) {
+  let element = document.querySelector(selector)
+  
+  return element
+}
+
+ function nestedTarget() {
+   let element = document.querySelector("#nested .target")
+  
+   return element
+ }
+
+ function increaseRankBy(n){
+  
+   let elementList = document.querySelectorAll("ul.ranked-list li")
+   for(var list = 0; list < elementList.length; list++) {
+      elementList[list].innerHTML = (parseInt(elementList[list])innerHTML + n).toString()
+    }
+ }
+
+ function deepestChild() {
+  
+ let element = document.querySelector("div#grand-node div div div div").innerHTML
+ 
+ return element
+}
+
+
+
+
+
+
+
 
